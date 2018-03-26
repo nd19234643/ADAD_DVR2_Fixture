b7a58820089c1161612e3fe7324783ea2b870a41
LeonHuang <nd19234643@gmail.com>
2018-03-26 16:00:53 +0800

    Add DEBUG_MODE token
    1.設定一個開關來控制 debug 訊息是否要輸出

--- DVR2Test/DVR2Test.ino
+++ DVR2Test/DVR2Test.ino
@@ -1,3 +1,12 @@
+#define DEBUG_MODE 1
+#ifdef DEBUG_MODE && DEBUG_MODE == 1
+#define DPrint(fmt, ...) Serial.print(fmt, ##__VA_ARGS__);
+#define DPrintln(fmt, ...) Serial.println(fmt, ##__VA_ARGS__);
+#else
+#define DPrint(...)
+#define DPrintln(...)
+#endif
+
 #define DELAY_TIME 10 // unit: ms
 #define BUFFER_SIZE 10
 
@@ -12,8 +21,9 @@
 #define BUZZER_SWITCH_INPIN 7 // 蜂鳴器 啟動/關閉
 
 boolean initFlag = false;
-unsigned char gpsCount = 0, checkFrequencyCount = 0, buzzerCount = 0;
+unsigned char gpsCount = 0, gprmcCount = 0, checkFrequencyCount = 0, buzzerCount = 0;
 int buzzerSwitchStatus = 0, startSwitchStatus = 0;
+unsigned long cycleTime = 0, oldTime = 0;
 
 char recvBuffer[BUFFER_SIZE] = {0}; // RX LDW or FCW data
 char warningMessage_1[7] = {'$', 'M', 'W', 'L', 'D', 'T', ','};   // $MWLDT,
@@ -23,6 +33,32 @@ char warningMessage_3[5] = {'A', 'T', '$', 'P', 'D'};             // AT$PD=21;LD
 unsigned char Period[3] = {0, 0, 0};
 unsigned char Pos[2] = {0, 0};
 unsigned char Neg[2] = {0, 0};
+
+/*
+$GPGGA,121252.000,3937.3032,N,11611.6046,E,1,05,2.0,45.9,M,-5.7,M,,0000*77 
+$GPRMC,121252.000,A,3958.3032,N,11629.6046,E,15.15,359.95,070306,,,A*54 
+$GPVTG,359.95,T,,M,15.15,N,28.0,K,A*04 
+$GPGGA,121253.000,3937.3090,N,11611.6057,E,1,06,1.2,44.6,M,-5.7,M,,0000*72 
+$GPGSA,A,3,14,15,05,22,18,26,,,,,,,2.1,1.2,1.7*3D 
+$GPGSV,3,3,10,29,07,074,,30,07,163,28*7D
+
+$GPRMC,<1>,<2>,<3>,<4>,<5>,<6>,<7>,<8>,<9>,<10>,<11>,<12>*hh
+<7> 地面速率（000.0~999.9節，前面的0也將被傳輸）
+*/
+
+/* 測速照相
+$GPRMC,05414.00,A,2350.7912,N,12029.0616,E,44.444,175.22,180517,,,A*6C
+$GPRMC,05414.00,A,2450.7384,N,12029.0568,E,44.444,175.22,180517,,,A*6C
+$GPRMC,05414.00,A,2450.5500,N,12029.05200,E,44.444,175.22,180517,,,A*6C
+$GPRMC,05414.00,A,2450.4966,N,12029.0496,E,44.444,175.22,180517,,,A*6C
+$GPRMC,05414.00,A,2450.3316,N,12029.05320,E,44.444,175.22,180517,,,A*6C
+*/
+// 53.4558(7) * 1.852 = 99 KM/H
+char *gprmcStr[5] = {"$GPRMC,05414.00,A,2350.7912,N,12029.0616,E,53.4558,175.22,180517,,,A*6C\r\n",
+                     "$GPRMC,05414.00,A,2450.7384,N,12029.0568,E,53.4558,175.22,180517,,,A*6C\r\n",
+                     "$GPRMC,05414.00,A,2450.5500,N,12029.05200,E,53.4558,175.22,180517,,,A*6C\r\n",
+                     "$GPRMC,05414.00,A,2450.4966,N,12029.0496,E,53.4558,175.22,180517,,,A*6C\r\n",
+                     "$GPRMC,05414.00,A,2450.3316,N,12029.05320,E,53.4558,175.22,180517,,,A*6C\r\n"};
 
 void setup()
 {
@@ -44,6 +80,11 @@ void loop()
 
     /***** Receive RX data from UART1 *****/
     receiveWarning(); // 10 ms
+
+    /***** Spend time for one loop cycle *****/
+    calculateCycleTime();
+    DPrint("Cycle Time = ");
+    DPrintln(cycleTime);
 
     /****** Check direction light frequency ******/
     int sensorVal_1 = digitalRead(DIRECTION_LIGHT_CHECK_INPIN); 
@@ -102,10 +143,19 @@ void initFlow()
       delay(200);
     }
 
+    oldTime = millis();
+
     buzzerSwitchStatus = digitalRead(BUZZER_SWITCH_INPIN);
     startSwitchStatus = digitalRead(START_SWITCH_INPIN);
     initFlag = true;
   }
+}
+
+void calculateCycleTime()
+{
+  unsigned long newTime = millis();
+  cycleTime = newTime - oldTime;
+  oldTime = newTime;
 }
 
 void checkSwitchStatus()
@@ -141,22 +191,28 @@ void sendGPSData()
   gpsCount++;
   if (gpsCount == 100) // 100 * DELAY_TIME = 1000 ms = 1 s
   {
-    Serial.println("Send GPS Data:");
-    Serial.println("----------------------------------------------------------------------");
-    Serial.print("$GPGGA,160530.00,2404.04359,N,12025.99974,E,1,06,2.28,-13.3,M,16.1,M,,*4E\r\n");
+    DPrintln("Send GPS Data:");
+    DPrintln("----------------------------------------------------------------------");
+    DPrint("$GPGGA,160530.00,2404.04359,N,12025.99974,E,1,06,2.28,-13.3,M,16.1,M,,*4E\r\n");
     Serial1.print("$GPGGA,160530.00,2404.04359,N,12025.99974,E,1,06,2.28,-13.3,M,16.1,M,,*4E\r\n");
-    Serial.println("----------------------------------------------------------------------");
+    DPrintln("----------------------------------------------------------------------");
 
     // GPS LED
     digitalWrite(GPS_TX_LED_OUTPIN, HIGH);
   } 
   else if (gpsCount >= 200) // 200 * DELAY_TIME = 2000 ms = 2s
   {
-    Serial.println("Send GPS Data:");
-    Serial.println("----------------------------------------------------------------------");
-    Serial.print("$GPRMC,160530.00,A,2404.04359,N,12025.99974,E,44.444,199.52,150617,,,A*72\r\n");
-    Serial1.print("$GPRMC,160530.00,A,2404.04359,N,12025.99974,E,44.444,199.52,150617,,,A*72\r\n");
-    Serial.println("----------------------------------------------------------------------");
+    DPrintln("Send GPS Data:");
+    DPrintln("----------------------------------------------------------------------");
+    // 測速照相測試，車速 99 km/h
+    DPrint(gprmcStr[gprmcCount]);
+    Serial1.print(gprmcStr[gprmcCount]);
+    gprmcCount++;
+    if (gprmcCount >= 5)
+    {
+      gprmcCount = 0;
+    }
+    DPrintln("----------------------------------------------------------------------");
 
     // GPS LED
     digitalWrite(GPS_TX_LED_OUTPIN, HIGH);
@@ -217,8 +273,8 @@ void receiveWarning()
 
       if (count == str3Length) {
         digitalWrite(WARNING_RX_LED_OUTPIN, HIGH);
-        Serial.println();
-        Serial.println("Success: Get warning message");
+        DPrintln();
+        DPrintln("Success: Get warning message");
         buzzerCount = 0;
       }
     }
@@ -300,8 +356,8 @@ void checkFrequencyRange(float lowValue, float highValue, int sensorVal)
   // Record count information for HIGH
   if (Pos[1] == 1)
   {
-//    Serial.print('%');
-//    Serial.println(Pos[0], DEC);
+//    DPrint('%');
+//    DPrintln(Pos[0], DEC);
     Period[0] = Pos[0]; 
     Pos[0] = 0;
     Pos[1] = 0; 
@@ -310,8 +366,8 @@ void checkFrequencyRange(float lowValue, float highValue, int sensorVal)
   // Record count information for LOW
   if (Neg[1] == 1)
   {
-//    Serial.print('%');
-//    Serial.println(Neg[0], DEC);
+//    DPrint('%');
+//    DPrintln(Neg[0], DEC);
     Period[1] = Neg[0];
     Neg[0] = 0;
     Neg[1] = 0;
@@ -325,18 +381,16 @@ void checkFrequencyRange(float lowValue, float highValue, int sensorVal)
     
     if ((Period[2] >= lowerBound) && (Period[2] <= upperBound))
     {
-      Serial.println("Success: Get direction light");
-      Serial.print('%');
-      Serial.println('0');
-//      Serial.println(Period[2], DEC);
+      DPrintln("Success: Get direction light"); // %0
+      DPrint("Count = ");
+      DPrintln(Period[2], DEC);
       digitalWrite(DIRECTION_LIGHT_LED_OUTPIN, HIGH);
     }
     else
     {
-      Serial.println("Failed: Get direction light");
-      Serial.print('%');
-      Serial.println('1');
-//      Serial.println(Period[2], DEC);
+      DPrintln("Failed: Get direction light"); // %1
+      DPrint("Count = ");
+      DPrintln(Period[2], DEC);
       digitalWrite(DIRECTION_LIGHT_LED_OUTPIN, LOW);
     }
 
